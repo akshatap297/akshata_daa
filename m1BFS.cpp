ccc6faa2d42a21bfbffb445dcdf54ee30e9e6b67
akshatap297 <akshatapawarap06@gmail.com>
2025-12-10 15:30:59 +0530

    Create m1BFS.cpp

---
+++ m1BFS.cpp
@@ -0,0 +1,39 @@
+#include &lt;iostream&gt;
+#include &lt;vector&gt;
+#include &lt;queue&gt;
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n, m;
+    cin >> n >> m;  
+    int g[n][100];    
+    int deg[n] = {0};  
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v; 
+        g[u][deg[u]++] = v;
+        g[v][deg[v]++] = u;
+    }
+    int visited[n] = {0};
+    int queue[1000];  
+    int front = 0, rear = 0;
+    int source;
+    cin >> source;
+ 
+    visited[source] = 1;
+    queue[rear++] = source;
+   cout << "BFS Traversal: ";
+    while(front < rear) {
+        int u = queue[front++];
+        cout << u << " ";
+        for(int i = 0; i < deg[u]; i++) {
+            int v = g[u][i];
+            if(!visited[v]) {
+                visited[v] = 1;
+                queue[rear++] = v;
+            }
+        }
+    }
+
+    return 0;
+}
