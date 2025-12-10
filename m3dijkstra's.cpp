ccfdd6311f7800835a47f75e899d9b8d457cf119
akshatap297 <akshatapawarap06@gmail.com>
2025-12-10 15:38:20 +0530

    Create m3dijkstra's.cpp

---
+++ m3dijkstra's.cpp
@@ -0,0 +1,50 @@
+#include &lt;iostream&gt;
+#include &lt;vector&gt;
+#include &lt;queue&gt;
+using namespace std;
+#define MAXN 100
+#define INF 1000000000
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int cost[MAXN][MAXN];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cost[i][j] = INF;
+
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        cost[u][v] = w;
+        cost[v][u] = w;
+    }
+    int source, target;
+    cin >> source >> target;
+    int dist[MAXN];
+    int visited[MAXN] = {0};
+    for (int i = 0; i < n; i++)
+        dist[i] = INF;
+    dist[source] = 0;
+    for (int count = 0; count < n; count++) {
+        int u = -1;
+        int minDist = INF;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && dist[i] < minDist) {
+                minDist = dist[i];
+                u = i;
+            }
+        }
+        if (u == -1) 
+                       break;
+        visited[u] = 1;
+        for (int v = 0; v < n; v++) {
+            if (!visited[v] && cost[u][v] != INF) {
+                if (dist[u] + cost[u][v] < dist[v])
+                    dist[v] = dist[u] + cost[u][v];
+            }
+        }
+    }
+    cout << "Fastest bypass to node " << target << " = " << dist[target] << endl;
+    return 0;
+}
