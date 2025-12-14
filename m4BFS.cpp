acd5f59422248bfad14e48a2ca89014eb2c1c5e1
akshatap297 <akshatapawarap06@gmail.com>
2025-12-14 10:25:29 +0530

    Update m4BFS.cpp

--- m4BFS.cpp
+++ m4BFS.cpp
@@ -1,19 +1,21 @@
-#include &lt;iostream&gt;
-#include &lt;vector&gt;
-#include &lt;queue&gt;
+#include <iostream>
 using namespace std;
-bool bfs(int m[10][10], int n, int source, int target) {
+bool bfs(int m[10][10], int n, int source, int target) 
+{
     int queue[20];
     int front = 0, rear = 0;
     int visited[10] = {0};
     queue[rear++] = source;
     visited[source] = 1;
-    while (front < rear) {
+    while (front < rear)
+        {
         int u = queue[front++];
         if (u == target) return true;
 
-        for (int v = 0; v < n; v++) {
-            if (m[u][v] == 1 && !visited[v]) {
+        for (int v = 0; v < n; v++)
+            {
+            if (m[u][v] == 1 && !visited[v]) 
+            {
                 visited[v] = 1;
                 queue[rear++] = v;
             }
@@ -23,9 +25,11 @@ bool bfs(int m[10][10], int n, int source, int target) {
     return false;
 }
 
-int main() {
+int main() 
+{
     int n = 5;
-    int m[10][10] = {
+    int m[10][10] =
+    {
         {0,1,0,0,0},
         {0,0,1,0,0},
         {0,0,0,0,0},
