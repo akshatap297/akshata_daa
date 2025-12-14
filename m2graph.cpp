bc680e0b75777b884e12eef0fae8bbe5ef24b00e
akshatap297 <akshatapawarap06@gmail.com>
2025-12-14 10:19:21 +0530

    Update m2graph.cpp

--- m2graph.cpp
+++ m2graph.cpp
@@ -1,13 +1,14 @@
-#include &lt;iostream&gt;
-#include &lt;vector&gt;
+#include <iostream>
 using namespace std;
-int main() {
+int main() 
+{
     int n, m;
     cin >> n >> m;
     int g[n][100];     
     int w[n][100];   
     int deg[n] = {0};  
-    for(int i = 0; i < m; i++) {
+    for(int i = 0; i < m; i++)
+        {
         int u, v, weight;
         cin >> u >> v >> weight;
         g[u][deg[u]] = v;
@@ -17,9 +18,11 @@ int main() {
         w[v][deg[v]] = weight;
         deg[v]++;
     }
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++)
+        {
         cout << i << ": ";
-        for(int j = 0; j < deg[i]; j++) {
+        for(int j = 0; j < deg[i]; j++) 
+        {
             cout << g[i][j] << "(" << w[i][j] << ") ";
         }
         cout << endl;
